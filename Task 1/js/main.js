b466f7e7a85a2f79e2544513221107f370b412fe
abhijeet141 <67696867+abhijeet141@users.noreply.github.com>
2021-07-05 02:56:56 +0530

    updated

--- Task 1/js/main.js
+++ Task 1/js/main.js
@@ -1,27 +1,28 @@
 $(".owl-carousel").owlCarousel({
-    merge:true,
-    loop:true,
-    video:true,
+  merge:true,
+  loop:true,
+  video:true,
+  image:true
 });
 
 // The marker, positioned at Uluru
 const marker = new google.maps.Marker({
+  position: uluru,
+  map: map,
+});
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 25.2861, lng: 82.9723 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
     position: uluru,
     map: map,
   });
-
-  // Initialize and add the map
-  function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 25.2861, lng: 82.9723 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
+}
